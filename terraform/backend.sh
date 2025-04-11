92ad2258477902f10fdba941d464a93a036ee1dc
nnreddybalreddy <narendra.mahn@gmail.com>
2025-04-11 10:26:43 -0400

    ALL

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -7,4 +7,6 @@ pip3.9 install botocore boto3
 #ansible-pull -i localhost, -U https://github.com/NarendraNReddy/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
 
 #ansible-pull -i localhost, -U https://github.com/NarendraNReddy1/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_Version
-ansible-pull -i localhost, -U https://github.com/NarendraNReddy1/expense-ansible-roles-tf-jenkins.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_Version
+# ansible-pull -i localhost, -U https://github.com/NarendraNReddy1/expense-ansible-roles-tf-jenkins.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_Version
+
+ansible-pull -i localhost, -U https://github.com/nnreddybalreddy/expense-ansible-roles2.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_Version
