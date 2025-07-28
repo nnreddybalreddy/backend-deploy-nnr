47d4957abcbb96382dd50e1ce953eedde800858f
nnreddybalreddy <narendra.mahn@gmail.com>
2025-07-28 00:41:04 -0400

    ALL

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.92.0"
+      version = "6.5.0"
     }
   }
     backend "s3" {
     bucket = "78s-remote-state-1"
-    key    = "remote-backend111-test222"
+    key    = "BACKEND-deploy_CICD_NNR24"
     region = "us-east-1"
     dynamodb_table="daws78s-locking"
   }
