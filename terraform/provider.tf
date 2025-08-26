510f5f3dc7b4f2faad20a96403cd87f31a885134
nnreddybalreddy <narendra.mahn@gmail.com>
2025-08-26 03:23:33 -0400

    ALL

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
     backend "s3" {
     bucket = "78s-remote-state-1"
-    key    = "Tools2222_CICD_NNR24"
+    key    = "wewew_CICD_NNR24"
     region = "us-east-1"
     dynamodb_table="daws78s-locking"
   }
